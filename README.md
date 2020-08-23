cab77633f8d8845e3dcd51f320863f2843a49da2
Jacobus Burger <therealjacoburger@gmail.com>
2020-08-22 23:25:55 -0700

    merged ssh setup into setup file and corrected typo

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # configurations
+This repository documents and tracks the configuration files (dotfiles) and environment/system setup scripts (setup) that I use to - in effect - make my environment portable.
+
+## dotfiles
+This directory contains configuration files for various programs I commonly use between my various systems, and a script to hard link these files to the current system (assuming that they are installed)
+
+## setup
+This directory contains various scripts that when run will setup the current running system for that particular system or environment.
+
+# Usage
+A typical setup on a fresh system will typically follow this procedure (starting at the root of the configurations repository tree)
+```bash
+$ cd dotfiles
+$ ./sync.sh
+$ cd ../
+```
+
---
+++ setup/environment/development/setup.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+# This script runs setup commands and automatically configures the environment for my programming/development environment
+
+
+
+
+
+echo """
+
+######################################
+# SETTING UP DEVELOPMENT ENVIRONMENT #
+######################################
+
+"""
+
+
+
+
+
+echo """
+
+##### SETTING UP PYTHON #####"""
+packages="neovim black flake8"
+if [[ -f `which pip3` ]]
+then
+  echo -e "\tupdating pip3 to latest version..."
+  pip3 install --upgrade pip
+  if [[ $packages != "" ]]
+  then
+    echo -e "\tinstalling packages for python..."
+    pip3 install $packages
+  fi
+else
+  echo """##### SKIPPING PYTHON, CABAL NOT INSTALLED #####"""
+fi
+echo """##### DONE #####"""
+
+
+
+
+
+echo """
+
+##### SETTING UP HASKELL #####"""
+packages=""
+if [[ -f `which cabal` ]]
+then
+  echo -e "\tupdating cabal to latest version..."
+  cabal update
+  cabal install cabal-install
+  if [[ $packages != "" ]]
+  then
+    echo -e "\tinstalling packages for haskell..."
+    cabal install $packages
+  fi
+else
+  echo """##### SKIPPING HASKELL, CABAL NOT INSTALLED #####"""
+fi
+echo """##### DONE #####"""
+
+
+
+
+
+echo """
+
+##### SETTING UP ZSH #####"""
+if [[ -f `which zsh` ]]
+then
+  echo "changing default shell for $(whoami) to zsh..."
+  sudo chsh -s /bin/zsh $(whoami)
+else
+  echo """##### SKIPPING ZSH, NOT INSTALLED #####"""
+fi
+echo """##### DONE #####"""
+
+
+
+
+
+echo """
+
+##### SETTING UP GIT / SSH #####"""
+if [[ -f `which git` ]]
+then
+  if [[ $(git config user.email) == "" ]]
+  then
+    echo "please set git user.email, skipping!"
+  else
+    email="$(git config user.email)"
+    if [[ ! -f "$HOME/.ssh/id_rsa" ]]
+    then
+      echo "git installed and email set, setting up ssh..."
+      ssh-keygen -t rsa -b 4096 -C "$email"
+      eval "$(ssh-agent -s)"
+      ssh-add ~/.ssh/id_rsa
+    else
+      echo "git installed, email set, and ssh setup, skipping..."
+    fi
+  fi
+  # start an ssh agent if it doesn't exist
+  ps -p $SSH_AGENT_PID > /dev/null || eval "$(ssh-agent -s)"
+else
+  echo """##### SKIPPING GIT, NOT INSTALLED #####"""
+fi
+echo """##### DONE #####"""

92b51d1416bc7869dea1b03bc909efe589f4ba04
Jacobus Burger <therealjacoburger@gmail.com>
2021-01-05 15:07:57 -0800

    updated setup script

--- setup/system/common/setup.sh
+++ setup/system/common/setup.sh
@@ -5,16 +5,3 @@
 
 # install common packages
 source distro/install.sh
-
-
-if [[ -f `which mpd` ]]
-then
-  echo "setting up mpd + ncmpcpp"
-  [[ ! -d $HOME/.config/mpd ]] && mkdir ~/.config/mpd
-  [[ ! -d $HOME/.local/share/mpd ]] && mkdir ~/.local/share/mpd
-  [[ ! -d $HOME/.config/ncmpcpp ]] && mkdir ~/.config/ncmpcpp
-else
-  echo "skipping mpd + ncmpcpp, not installed"
-fi
-# mpd # mpd is automatically set to autostart on user login
-# you can check if it worked with `tail ~/.local/share/mpd/mpd.log`

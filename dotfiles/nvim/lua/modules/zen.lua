c75b7b14465dd6d08316ab6ced13c8a9d1484273
Jacobus Burger <therealjacoburger@gmail.com>
2023-06-04 14:05:37 -0700

    added zen module

---
+++ dotfiles/nvim/lua/modules/zen.lua
@@ -0,0 +1,93 @@
+-- zen-mode: distraction-free coding for neovim
+-- twilight: dims inactive portions of the code
+-- see:
+-- * zen-mode: https://github.com/folke/zen-mode.nvim
+-- * twilight: https://github.com/folke/twilight.nvim
+return {
+  -- zen-mode config
+  {
+    'folke/zen-mode.nvim',
+    dependencies = {
+      'folke/twilight.nvim',
+    },
+    cmd = "ZenMode",
+    opts = {
+      window = {
+        backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+        -- height and width can be:
+        -- * an absolute number of cells when > 1
+        -- * a percentage of the width / height of the editor when <= 1
+        -- * a function that returns the width or the height
+        width = .85, -- width of the Zen window
+        height = 1, -- height of the Zen window
+        -- by default, no options are changed for the Zen window
+        -- uncomment any of the options below, or add other vim.wo options you want to apply
+        options = {
+          -- signcolumn = "no", -- disable signcolumn
+          -- number = false, -- disable number column
+          -- relativenumber = false, -- disable relative numbers
+          -- cursorline = false, -- disable cursorline
+          -- cursorcolumn = false, -- disable cursor column
+          -- foldcolumn = "0", -- disable fold column
+          -- list = false, -- disable whitespace characters
+        },
+      },
+      plugins = {
+        -- disable some global vim options (vim.o...)
+        -- comment the lines to not apply the options
+        options = {
+          enabled = true,
+          ruler = false, -- disables the ruler text in the cmd line area
+          showcmd = false, -- disables the command in the last line of the screen
+        },
+        twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+        gitsigns = { enabled = false }, -- disables git signs
+        -- this will change the font size on alacritty when in zen mode
+        -- requires  Alacritty Version 0.10.0 or higher
+        -- uses `alacritty msg` subcommand to change font size
+        alacritty = {
+          enabled = true,
+          font = "16", -- font size
+        },
+        -- this will change the font size on wezterm when in zen mode
+        -- See alse also the Plugins/Wezterm section in this projects README
+        wezterm = {
+          enabled = false,
+          -- can be either an absolute font size or the number of incremental steps
+          font = "+4", -- (10% increase per step)
+        },
+      },
+      -- callback where you can add custom code when the Zen window opens
+      on_open = function(win)
+      end,
+      -- callback where you can add custom code when the Zen window closes
+      on_close = function()
+      end,
+    },
+  },
+  -- twilight config
+  {
+    'folke/twilight.nvim',
+    lazy = true,
+    opts = {
+      dimming = {
+        alpha = 0.25, -- amount of dimming
+        -- we try to get the foreground from the highlight groups or fallback color
+        color = { "Normal", "#ffffff" },
+        term_bg = "#000000", -- if guibg=NONE, this will be used to calculate text color
+        inactive = true, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
+      },
+      context = 13, -- amount of lines we will try to show around the current line
+      treesitter = true, -- use treesitter when available for the filetype
+      -- treesitter is used to automatically expand the visible text,
+      -- but you can further control the types of nodes that should always be fully expanded
+      expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
+        "function",
+        "method",
+        "table",
+        "if_statement",
+      },
+      exclude = {}, -- exclude these filetypes
+    },
+  },
+}

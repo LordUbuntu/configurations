bfa8a6848bcfadc23986942f10f6d922b94f2c0e
Jacobus Burger <therealjacoburger@gmail.com>
2023-06-05 23:50:34 -0700

    made module lazier

--- dotfiles/nvim/lua/modules/bufferline.lua
+++ dotfiles/nvim/lua/modules/bufferline.lua
@@ -3,9 +3,9 @@
 -- see: https://github.com/akinsho/bufferline.nvim
 return {
   'akinsho/bufferline.nvim',
+  event = 'VeryLazy',
   dependencies = 'nvim-tree/nvim-web-devicons',
   version = '*',
-  event = 'VeryLazy',
   config = function()
     require('bufferline').setup({})
   end,

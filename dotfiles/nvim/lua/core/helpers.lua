b0d25cb9267d70afbf2a89b41c3d3d426c16cbde
LordUbuntu <therealjacoburger@gmail.com>
2023-06-16 22:28:56 -0700

    added keymaps and pending future keymap

--- dotfiles/nvim/lua/core/helpers.lua
+++ dotfiles/nvim/lua/core/helpers.lua
@@ -2,17 +2,22 @@ local M = {}
 
 -- map keys
 M.map = function(mode, lhs, rhs, desc)
-  vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
-  -- local ok, whichkey = pcall(require, 'which-key')
-  -- if ok then
-  --   whichkey.register({
-  --     [lhs] = { name = desc },
-  --   }, { mode = mode })
-  -- end
+  vim.keymap.set(mode, lhs, rhs, { silent = true, buffer = bufnr, desc = desc })
+end
+
+-- TODO: future map replacement
+-- NOTE: opts is a table of options like buffer, desc, silent, etc
+M.mmap = function(mode, lhs, rhs, opts)
+  opts.silent = opts.silent or true
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+M.lsp_map = function(lhs, rhs, bufnr, desc)
+  vim.keymap.set('n', lhs, rhs, { silent = true, buffer = bufnr, desc = desc })
 end
 
 -- attempt to get colorscheme if available
-M.theme = function(name)
+M.try_colorscheme = function(name)
   local ok, _ = pcall(vim.cmd.colorscheme, name)
   if ok then
     return name

915839de49f3c6c4110511824174ace5bf8583d6
LordUbuntu <therealjacoburger@gmail.com>
2023-08-22 11:54:17 -0700

    added git bootstrap

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-
 echo """
+
 ======= GIT =======
 """
+# install git if it is not installed already
 if [ ! $(command -v git) ]
 then
   echo "GIT not installed, attempting to install GIT..."
   # "install git" or something using host-wrapper install function
 fi
+# setup git if/once it is installed
 if [ $(command -v git) ]
 then
   # setup git configurations
+  read -rp "git user email: " email
+  git config --global user.email "$email"
 else
   echo "!!! GIT was not installed, may need to be installed manually..."
 fi
+
+
+

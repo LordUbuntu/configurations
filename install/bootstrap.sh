578613473b81b48828e66fe1b12f477dfa330072
LordUbuntu <therealjacoburger@gmail.com>
2023-08-22 12:08:46 -0700

    added ssh setup

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+# TODO: the install checks at the start of each should be moved into a func
+
+
+
+echo """
+#################
+# BOOTSTRAPPING #
+#################
+"""
+
+
 
 echo """
 
@@ -15,13 +26,40 @@ if [ $(command -v git) ]
 then
   if [ $(git config user.email) == "" ]
   then
-    read -rp "git user email: " email
+    read -rp "Git user email: " email
     git config --global user.email "$email"
   fi
 else
-  echo "!!! GIT was not installed, may need to be installed and setup manually..."
+  echo "!!! GIT failed !!!"
   exit 1
 fi
 
 
 
+echo """
+
+======= SSH =======
+"""
+# install ssh if it is not installed already
+if [ ! $(command -v ssh) ]
+then
+  echo "SSH not installed, attempting to install SSH..."
+  # "install ssh" or something using host-wrapper install function
+fi
+if [ $(command -v ssh) ]
+then
+  if [ ! -f "$HOME/.ssh/id_rsa" ]
+  then
+    echo "Forging SSH keys for $USER with git email $(git config user.email)"
+    ssh-keyget -t rsa -b 4096 -C "$(git config user.email)"
+    ssh-add ~/.ssh/id_rsa
+  fi
+  if [ ! $(ps -p "$SSH_AGENT_PID" >/dev/null) ]
+  then
+    echo "Staring SSH agent"
+    eval "$(ssh-agent -s)"
+  fi
+else
+  echo "!!! SSH failed !!!"
+  exit 1
+fi

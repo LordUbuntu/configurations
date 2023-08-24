4745080a478836721f771fb304d222e76ed1b3bc
LordUbuntu <therealjacoburger@gmail.com>
2023-08-23 17:29:29 -0700

    added treesitter and cargo setup script

--- dotfiles/nvim/deps.sh
+++ dotfiles/nvim/deps.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # install dependencies for nvim
+echo "========== INSTALLING - NVIM =========="
+
 
 
 # ensure prerequisites met
@@ -10,11 +12,22 @@ then
 fi
 
 
-# TODO:
-# - treesitter
-#   - rust (cargo)
-#   - tree-sitter-cli (`cargo install tree-sitter-cli`)
-# - go build chain (darkman)
+
+# install and setup treesitter
+# install and setup rust build tools
+if [ ! $(command -v cargo) ]
+then
+  echo "Installing rust"
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+if [ ! $(command -v treesitter) ]
+then
+  echo "Installing treesitter"
+  export PATH="$PATH:$HOME/.cargo/bin"
+  cargo install tree-sitter-cli
+fi
+
+
 
 # install and setup pyenv to give neovim a Python 3 provider with virutalenv
 if [ ! -d "$HOME/.pyenv" ]
@@ -22,22 +35,33 @@ then
   echo "Installing pyenv"
   PYENV_GIT_TAG=v2.3.24 
   curl https://pyenv.run | bash
-else
-  if [ -f "$HOME/.pyenv/bin/pyenv" ]
-  then
-    echo "Setting up neovim pyenv"
-    # put pyenv in path if it exists but isn't in it
-    if [ ! $(command -v pyenv) ]
-    then
-      export PATH="$PATH:$HOME/.pyenv/bin/pyenv"
-    fi
-    # install and create virtualenv
-    pyenv install 3.10
-    pyenv virtualenv neovim3
-    # install packages in virtual env
-    echo "Setting up neovim3 virtual env"
-    pyenv activate neovim3
-    pip install pynvim neovim
-    source deactivate
-  fi
 fi
+if [ -f "$HOME/.pyenv/bin/pyenv" ]
+then
+  echo "Setting up neovim pyenv"
+  # put pyenv in path if it exists but isn't in it
+  if [ ! $(command -v pyenv) ]
+  then
+    export PATH="$PATH:$HOME/.pyenv/bin/pyenv"
+  fi
+  # install and create virtualenv
+  pyenv install 3.10
+  pyenv virtualenv neovim3
+  # install packages in virtual env
+  echo "Setting up neovim3 virtual env"
+  pyenv activate neovim3
+  pip install pynvim neovim
+  source deactivate
+fi
+
+
+
+# TODO:
+# - go build chain (darkman)
+
+
+
+
+# finished
+echo "Set up dependencies for neovim, PATH=$PATH"
+echo "===================="

08ff10aa29e810740c244c77a3bfcd3d6de27e6d
LordUbuntu <therealjacoburger@gmail.com>
2023-08-23 23:57:54 -0700

    completed nvim setup script

--- dotfiles/nvim/setup.sh
+++ dotfiles/nvim/setup.sh
@@ -1,43 +1,43 @@
 #!/bin/sh
 # install dependencies for nvim
-echo "\e[34m========== INSTALLING - NVIM =========="
+echo "\e[34m========== INSTALLING - NVIM ==========\e[0m"
 
 
 
 # ensure prerequisites met
-echo "\e[34m===== Checking prerequisites"
+echo "\e[34m===== Checking prerequisites\e[0m"
 if [ ! $(command -v curl) ]
 then
-  echo "\e[31mCRITICAL: curl is missing"
+  echo "\e[31mCRITICAL: curl is missing\e[0m"
   exit 1
 fi
 
 
 
 # install and setup tree-sitter
-echo "\e[34m===== Setting up tree-sitter"
+echo "\e[34m===== Setting up tree-sitter\e[0m"
 PATH="$PATH:$HOME/.cargo/bin"  # setup path to avoid reinstalls
 if [ ! $(command -v cargo) ]
 then
-  echo "\e[33mInstalling rust"
+  echo "\e[33mInstalling rust\e[0m"
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
   if [ $? ]
   then
-    echo "\e[33mDone rust"
+    echo "\e[33mDone rust\e[0m"
   else
-    echo "\e[31mCRITICAL: failed to install rust"
+    echo "\e[31mCRITICAL: failed to install rust\e[0m"
     exit 1
   fi
 fi
 if [ ! $(command -v tree-sitter) ]
 then
-  echo "\e[33mInstalling tree-sitter"
+  echo "\e[33mInstalling tree-sitter\e[0m"
   cargo install tree-sitter-cli
   if [ $? ]
   then
-    echo "\e[33mDone tree-sitter"
+    echo "\e[33mDone tree-sitter\e[0m"
   else
-    echo "\e[31mCRITICAL: failed to install tree-sitter"
+    echo "\e[31mCRITICAL: failed to install tree-sitter\e[0m"
     exit 1
   fi
 fi
@@ -49,21 +49,22 @@ echo "\e[34m===== Setting up Python 3 virutual environment"
 PATH="$PATH:$HOME/.pyenv/bin/pyenv"  # setup path to avoid reinstalls
 if [ ! $(command -v pyenv) ]
 then
-  echo "\e[33mInstalling pyenv"
+  echo "\e[33mInstalling pyenv\e[0m"
   PYENV_GIT_TAG=v2.3.24 
   curl https://pyenv.run | bash
   if [ $? ]
   then
-    echo "\e[32mDone pyenv"
+    echo "\e[32mDone pyenv\e[0m"
   else
-    echo "\e[31mCRITICAL: failed to install pyenv"
+    echo "\e[31mCRITICAL: failed to install pyenv\e[0m"
     exit 1
   fi
+  unset PYENV_GIT_TAG
 fi
 # setup neovim3 pyenv if not already
 if [ ! $(pyenv virtualenvs | grep 'neovim3' | awk '{print $1}') ]
 then
-  echo "\e[33mSetting up neovim3 virtual env"
+  echo "\e[33mSetting up neovim3 virtual env\e[0m"
   # install and create virtualenv
   pyenv install 3.10
   pyenv virtualenv neovim3
@@ -73,20 +74,46 @@ then
   source deactivate
   if [ $? ]
   then
-    echo "\e[32mDone virtualenv for neovim"
+    echo "\e[32mDone virtualenv for neovim\e[0m"
   else
-    echo "\e[31mCRITICAL: failed to setup virutalenv for neovim"
+    echo "\e[31mCRITICAL: failed to setup virutalenv for neovim\e[0m"
     exit 1
   fi
 fi
 
 
 
-# TODO:
-# - go build chain (darkman)
+# install and setup go for darkman
+echo "\e[34m===== Setting up darkman\e[0m"
+PATH="$PATH:/usr/local/go/bin"  # setup path to avoid reinstalls
+VERSION=1.21.0
+ARCH=amd64
+if [ ! $(command -v go) ]
+then
+  echo "\e[33mInstalling go\e[0m"
+  # download latest
+  curl -O -L "https://golang.org/dl/go$VERSION.linux-$ARCH.tar.gz"
+  # remove any previous installations and untar to /usr/local/go.
+  # DO NOT UNTAR INTO EXISTING GO INSTALLATIONS
+  echo "\e[31mNEED SUDO TO INSTALL GO:\e[0m"
+  echo "\e[31msudo rm -rf /usr/local/go\e[0m"
+  echo "\e[31msudo tar -C /usr/local -xzf \"go$VERSION.linux-$ARCH.tar.gz\"\e[0m"
+  sudo rm -rf /usr/local/go
+  sudo tar -C /usr/local -xzf "go$VERSION.linux-$ARCH.tar.gz"
+  # clean up
+  rm "go$VERSION.linux-$ARCH.tar.gz"
+  if [ $? ]
+  then
+    echo "\e[32mDone installing go\e[0m"
+  else
+    echo "\e[31mCRITICAL: failed to install go\e[0m"
+    exit 1
+  fi
+fi
+unset VERSION ARCH
 
 
 
 # finished
-echo "\e[33mfinal PATH=$PATH"
-echo "\e[34m======================================="
+echo "PATH=$PATH"
+echo "\e[34m=======================================\e[0m"

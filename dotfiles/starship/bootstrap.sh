638ac9bc1ef962e19a9c9c6e590f8866a57e72a1
Jacobus Burger <therealjacoburger@gmail.com>
2023-09-13 13:11:27 -0700

    add dependencies

--- dotfiles/starship/bootstrap.sh
+++ dotfiles/starship/bootstrap.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 # Jacobus Burger (2023)
+echo -e "\e[34m===== BOOTSTRAP - STARSHIP\e[0m"
+
+
 
 ##### Install Dependencies if not on nix
 if [ ! $(command -v nix) ]
 then
-  echo -e "\e[34m===== INSTALL DEPENDENCIES - STARSHIP\e[0m"
+  # ensure rustup
+  if [ ! $(command -v cargo) ]
+  then
+    echo -e "\e[34m===== INSTALL RUST\e[0m"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  fi
+  # install starship itself
+  if [ ! $(command -v starship) ]
+  then
+    echo -e "\e[34m===== INSTALL STARSHIP\e[0m"
+    cargo install starship
+  fi
 fi
-
-
-
-##### Setup Environment
 
 
 

bfbb50aa7c9bf67dd1b92a532f1e2ddcad5a1fd5
Jacobus Burger <therealjacoburger@gmail.com>
2023-09-12 17:12:24 -0700

    added functions to ease development

---
+++ install/functions.sh
@@ -0,0 +1,16 @@
+function link {
+  src=$1
+  dst=$2
+  echo "\e[34m===== Linking $src to $dst\e[0m"
+  read -p "This will overwrite all files at $(dirname $dst), continue?" ans
+  case $ans in
+    [Yy]* )
+      echo "copying from $PWD/default to $dst"
+      rm -rf "$dst"
+      cp -R "$PWD/default/." "$dst"
+      break;;
+  * ) echo "Skipping..."
+  esac
+}
+
+export -f link

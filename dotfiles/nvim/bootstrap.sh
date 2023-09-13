c04fa35fcd6c483afb73f3d1569845824d249dc1
Jacobus Burger <therealjacoburger@gmail.com>
2023-09-12 17:25:28 -0700

    improving script flow

--- dotfiles/nvim/bootstrap.sh
+++ dotfiles/nvim/bootstrap.sh
@@ -4,15 +4,15 @@
 ##### Install Dependencies if not on nix
 if [ ! $(command -v nix) ]
 then
-  echo "\e[34m===== INSTALL DEPENDENCIES - NVIM\e[0m"
+  echo -e "\e[34m===== INSTALL DEPENDENCIES - NVIM\e[0m"
   if [ ! $(command -v cargo) ]
   then
-    echo "\e[34m===== INSTALL RUST\e[0m"
+    echo -e "\e[34m===== INSTALL RUST\e[0m"
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
   fi
   if [ ! $(command -v tree-sitter) ]
   then
-    echo "\e[34m===== INSTALL TREESITTER\e[0m"
+    echo -e "\e[34m===== INSTALL TREESITTER\e[0m"
     cargo install tree-sitter-cli
   fi
 fi
@@ -20,12 +20,12 @@ fi
 
 
 ##### Setup Environment
-echo "\e[34m===== BOOTSTRAP ENVIRONMENT - NVIM\e[0m"
+echo -e "\e[34m===== BOOTSTRAP ENVIRONMENT - NVIM\e[0m"
 # install and setup neovim virtual environment
-echo "\e[34m===== Setting up Python 3 virutual environment"
+echo -e "\e[34m===== Setting up Python 3 virutual environment"
 if [ ! -d "$HOME/.virtualenvs/neovim3" ]
 then
-  echo "\e[33mSetting up neovim3 virtual env\e[0m"
+  echo -e "\e[33mSetting up neovim3 virtual env\e[0m"
   # setup virutal environment path
   python -m venv "$HOME/.virtualenvs/neovim3"
   source "$HOME/.virtualenvs/neovim3/bin/activate"
@@ -34,9 +34,9 @@ then
   deactivate
   if [ $? ]
   then
-    echo "\e[32mDone virtualenv for neovim\e[0m"
+    echo -e "\e[32mDone virtualenv for neovim\e[0m"
   else
-    echo "\e[31mCRITICAL: failed to setup virutalenv for neovim\e[0m"
+    echo -e "\e[31mCRITICAL: failed to setup virutalenv for neovim\e[0m"
     exit 1
   fi
 fi
@@ -44,17 +44,17 @@ fi
 
 
 # install and setup fzf
-echo "\e[34m===== Setting up fzf\e[0m"
+echo -e "\e[34m===== Setting up fzf\e[0m"
 if [ ! $(command -v fzf) ]
 then
-  echo "\e[33mInstalling fzf\e[0m"
+  echo -e "\e[33mInstalling fzf\e[0m"
   git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
   $HOME/.fzf/install
   if [ $? ]
   then
-    echo "\e[32mDone installing fzf\e[0m"
+    echo -e "\e[32mDone installing fzf\e[0m"
   else
-    echo "\e[31mCRITICAL: failed to install fzf\e[0m"
+    echo -e "\e[31mCRITICAL: failed to install fzf\e[0m"
     exit 1
   fi
 fi
@@ -66,4 +66,4 @@ source ../../install/functions.sh
 link "$PWD/default/." "$HOME/.config/nvim"
 
 
-echo "\e[34m=======================================\e[0m"
+echo -e "\e[34m=======================================\e[0m"
--- install/functions.sh
+++ install/functions.sh
@@ -1,7 +1,7 @@
 function link {
   src=$1
   dst=$2
-  echo "\e[34m===== Linking $src to $dst\e[0m"
+  echo -e "\e[34m===== Linking $src to $dst\e[0m"
   read -p "This will overwrite all files at $(dirname $dst), continue?" ans
   case $ans in
     [Yy]* )

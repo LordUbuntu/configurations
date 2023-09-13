4e26d1a86ac9cebc5ab4ea5fab4c28cd84d60c81
Jacobus Burger <therealjacoburger@gmail.com>
2023-09-12 17:30:26 -0700

    unify bootstrap

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -14,7 +14,41 @@ echo """
 if [ ! $(command -v nix) ]
 then
   # install dotfile dependencies
-  sh deps.sh
+  # global dependencies
+  # unzip
+  # wget
+  # curl
+  # gzip
+  # tar
+  # bash
+  # sh
+  # git
+  # gcc
+
+  programs="
+  bash
+  curl
+  gcc
+  git
+  gzip
+  sh
+  tar
+  unzip
+  wget
+  "
+
+  if [ $(command -v apt) ]
+  then
+    # substitute programs with weird names like bat -> bat-cat
+    # install packages
+    sudo apt install "$programs"
+  elif [ $(command -v dnf) ]
+  then
+    sudo dnf install "$programs"
+  elif [ $(command -v pacman) ]
+  then
+    sudo pacman -suy "$programs"
+  fi
 fi
 
 

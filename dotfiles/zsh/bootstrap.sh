231af4b0a622122ecdf4ea570e17b10bc2977ed0
Jacobus Burger <therealjacoburger@gmail.com>
2023-09-14 22:23:13 -0700

    added bootstrap

---
+++ dotfiles/zsh/bootstrap.sh
@@ -0,0 +1,20 @@
+echo -e "\e[34m===== BOOTSTRAP - BAT\e[0m"
+
+
+
+##### Install Dependencies if not on nix
+if [ ! $(command -v nix) ]
+then
+  echo -e "\e[34m===== INSTALL DEPENDENCIES - BAT\e[0m"
+else
+  echo "please ensure duf, exa, ripgrep, ripgrep-all, zellij, nvim, cargo, fzf, fasd, nnn, starship, and unimatrix"
+fi
+
+
+
+##### Link Files
+source general/functions.sh
+link "$PWD/dotfiles/zsh/zshrc" "$HOME/.zshrc"
+
+
+echo -e "\e[34m=======================================\e[0m"

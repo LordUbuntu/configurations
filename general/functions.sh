23e213448c0341d75366377639c3bda05a7f747c
Jacobus Burger <therealjacoburger@gmail.com>
2023-09-16 23:44:56 -0700

    don't prompt for confirmation with every single link

--- general/functions.sh
+++ general/functions.sh
@@ -2,14 +2,10 @@ function link {
   src=$1
   dst=$2
   echo "Linking $src to $dst"
-  read -p "This will overwrite all files at $dst, continue?" ans
-  case $ans in
-    [Yy]* )
-      echo "copying from $src to $dst"
-      rm -rf "$dst"
-      [ ! -d "$(dirname $dst)" ] && mkdir -p "$(dirname $dst)"
-      [ -d "$src" ] && cp -R "$src"/. "$dst" || cp -R "$src" "$dst"
-      break;;
-  * ) echo "Skipping..."
-  esac
+  # 1. backup preexising files
+  # 2. make program directory if it doesn't exist
+  # 3. copy $src directory or $src file to $dst
+  [ -e "$dst" ] && cp "$dst" "$dst.bak"  # backup preexisting files
+  [ ! -d "$(dirname $dst)" ] && mkdir -p "$(dirname $dst)"
+  [ -d "$src" ] && cp -R "$src"/. "$dst" || cp -R "$src" "$dst"
 }
